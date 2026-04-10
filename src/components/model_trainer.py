561a6314971eeebed327f6833c06b9c2d5e8d904
Arjun Pant <pantarjun234@gmail.com>
2026-04-10 21:51:34 +0530

    Model Training Done

---
+++ src/components/model_trainer.py
@@ -0,0 +1,82 @@
+import os 
+import sys
+from dataclasses import dataclass
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_obj,evaluate_models
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts',"model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Start Doing train and test split")
+
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models={
+                "Random Forest Regressor": RandomForestRegressor(),
+                "GradientBoost Regressor": GradientBoostingRegressor(),
+                "Decision Tree": DecisionTreeRegressor()
+            }
+
+            params={
+                "Random Forest Regressor":{
+                    "max_depth": [5, 8, 15, None, 10],
+                    "max_features": [5, 7, None, 8],
+                    "min_samples_split": [2, 8, 15, 20],
+                    "n_estimators": [100, 200, 500, 1000]},
+                "GradientBoost Regressor":{
+                    "loss": ['squared_error','huber','absolute_error'],
+                    "criterion": ['friedman_mse','squared_error'],
+                    "min_samples_split": [2, 8, 15, 20],
+                    "n_estimators": [100, 200, 500],
+                    "max_depth": [5, 8, 15, None, 10],
+                },
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
+
+            best_model_score=max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model=models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No Best Model found for this dataset")
+            
+            logging.info("Best Model is found for the training and test dataset")
+
+            save_obj(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            score=r2_score(y_test,predicted)
+            return score
+        except Exception as e:
+            raise CustomException(e,sys)
